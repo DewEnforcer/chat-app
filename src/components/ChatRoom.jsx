94c1e5a0f32471595f05ad2248e5810463b8a06c
DewEnforcer <piranha863@gmail.com>
2021-02-16 12:01:15 +0100

    Chat handlers raised to chatroom component, chatroom component reworked into stateful component

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,28 +1,71 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Component } from 'react'
 import { Prompt } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 import ChatUserContext from '../context/ChatUserContext';
+import createChatSocket from '../sockets/createChatSocket';
 import Chat from './chat/Chat'
 import InviteBar from './chat/InviteBar'
 import UsersOverview from './chat/UsersOverview';
 
-export default function ChatRoom({history}) {
-    const chatRoomEl = useRef();
-    const [user, setUser] = useState();
-    const [chatUsers, setChatUsers] = useState([]);
-    const token = history.location.state.chatId;
+export default class ChatRoom extends Component {
+    state = {
+        roomMsgs: [],
+        chatUsers: [],
+        chatName: "",
+        connected: false,
+        socket: null
+    }
+    
+    token = this.props.history.location.state.chatId;
+    user = this.props.history.location.state.user;
 
-    useEffect(() => {
-        setUser(history.location.state.user);
-    }, [])
+    handleMessageSubmit = text => this.state.socket.emit("msgRequest", {text});
 
-    return (
-        <div className="chat_room" ref={chatRoomEl}>
+    handleConnect = () => {
+        this.setState({...this.state, connected: true});
+        this.state.socket.emit("connectRequest", {chatId: this.token, user: this.user});
+    }
+
+    handleChatData = ({msgs, chatName}) => {
+        this.setState({...this.state, roomMsgs: msgs, chatName});
+    }
+
+    handleChatUsers = data => this.setState({...this.state, chatUsers: data});
+
+    handleMsg = msg => {
+        if (!msg) return;
+        
+        const newState = {...this.state};
+        newState.roomMsgs = [...newState.roomMsgs, msg];
+        this.setState(newState);
+    }
+
+    handleDisconnect = (data) => {
+        if (!data.includes("client")) toast.error("Lost connection to the server, please wait for connection to be reestablished or create a new chat room.");
+    }
+
+    componentDidMount() {
+        const socket = createChatSocket("http://localhost:5000", this.handleConnect, this.handleChatData, this.handleChatUsers, this.handleMsg, this.handleDisconnect);
+        this.setState({...this.state, socket});
+    }
+
+    componentWillUnmount() {
+        this.state.socket.disconnect();
+    }
+
+    render() {
+        const {chatName, roomMsgs, connected, chatUsers} = this.state;
+
+        return (
+        <div className="chat_room">
             <Prompt message="Are you sure you want to leave this chatroom?"/>
-            <ChatUserContext.Provider value={user}>
-                <InviteBar chatId={token}/>
-                <Chat setChatUsers={setChatUsers} chatId={token} user={user}/>
+            <ChatUserContext.Provider value={this.user}>
+                <InviteBar chatId={this.token}/>
+                <Chat onSubmit={this.handleMessageSubmit} chatName={chatName} roomMsgs={roomMsgs} connected={connected}/>
                 <UsersOverview users={chatUsers}/>
             </ChatUserContext.Provider>
         </div>
-    )
+        )
+    }
 }
--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,84 +1,32 @@
-import React, { Component } from 'react'
-import {io} from "socket.io-client";
-import { toast } from 'react-toastify';
+import React, { useState } from 'react'
 
 import Loader from '../Loader';
 import TypeBar from '../TypeBar'
 import ChatHeader from './ChatHeader';
 import MessageList from './MessageList'
-import Status from './Status'
-import createChatSocket from '../../sockets/createChatSocket';
 
-export default class Chat extends Component {
-    state = {
-        roomMsgs: [],
-        input: "",
-        connected: false,
-        activity: null,
-        socket: null,
-        chatName: ""
-    }
-
-    handleMessageType = val => {
-        const newState = {...this.state};
-        newState.input = val;
-        this.setState(newState);
-    }
-
-    handleMessageSubmit = () => {
-        const {socket, input} = this.state;
+export default function Chat({onSubmit, chatName, roomMsgs, connected}) {
+    const [input, setInput] = useState("");
+    
+    const handleMessageSubmit = () => {
         if (input.length <= 0) return;
-
-        socket.emit("msgRequest", {text: input});
-
-        this.setState({...this.state, input: ""});
+        
+        onSubmit(input);
+        
+        setInput("");
     }
 
-    handleConnect = () => {
-        let chatId = this.props.chatId;
-        let user = this.props.user;
-        this.setState({...this.state, connected: true});
-        this.state.socket.emit("connectRequest", {chatId, user});
+    const handleInputChange = val => {
+        setInput(val);
     }
-
-    handleChatData = ({msgs, chatName}) => {
-        this.setState({...this.state, roomMsgs: msgs, chatName});
-    }
-
-    handleChatUsers = data => this.props.setChatUsers(data)
-
-    handleMsg = msg => {
-        if (!msg) return;
-        const newState = {...this.state};
-        newState.roomMsgs = [...newState.roomMsgs, msg];
-        this.setState(newState);
-    }
-
-    handleDisconnect = (data) => {
-        if (!data.includes("client")) toast.error("Lost connection to the server, please wait for connection to be reestablished or create a new chat room.");
-    }
-
-    componentDidMount() {
-        const socket = createChatSocket("http://localhost:5000", this.handleConnect, this.handleChatData, this.handleChatUsers, this.handleMsg, this.handleDisconnect);
-        this.setState({...this.state, socket});
-    }
-
-    componentWillUnmount() {
-        this.state.socket.disconnect();
-    }
-
-    render() {
-        const {roomMsgs, input, activity, connected, chatName} = this.state;
-
-        if (!connected) return <Loader title="Connecting..."/>
-
-        return (
+    
+    if (!connected) return <Loader title="Connecting..."/>
+    
+    return (
         <div className="chat_wrapper flex_column">
             <ChatHeader title={chatName}/>
             <MessageList msgs={roomMsgs}/>
-            {activity && <Status statusId={activity.statusId} author={activity.user}/>}
-            <TypeBar onChange={this.handleMessageType} onSubmit={this.handleMessageSubmit} value={input}/>
+            <TypeBar onChange={handleInputChange} onSubmit={handleMessageSubmit} value={input}/>
         </div>
-        )
-    }
+    )
 }
